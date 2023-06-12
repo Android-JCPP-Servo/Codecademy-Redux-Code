23933f1ee95d5038faca386df2fe6bfc61ab68b3
Android-JCPP-Servo <astewart1138@gmail.com>
2023-06-12 18:33:54 -0500

    Add bad code to compare with good code

--- pure.js
+++ pure.js
@@ -1,6 +1,19 @@
 /**
  * If a function is pure, then it will always have the same outputs given the same inputs.
  */
+
+// BAD!!!
+const fs = require('fs');
+const file = './data.txt';
+
+const capitalizeMessage = (file) => {
+  const message = fs.readFileSync(file, 'utf8');
+  return message.toUpperCase();
+}
+
+console.log(capitalizeMessage(file));
+
+// GOOD!!!
 const fs = require('fs');
 const file = './data.txt';
 
