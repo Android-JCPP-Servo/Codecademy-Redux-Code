e79a8bef502986af65501e633983b43680ddc262
Android-JCPP-Servo <astewart1138@gmail.com>
2023-06-12 18:33:18 -0500

    Add bad code to compare with good code

--- immutable.js
+++ immutable.js
@@ -2,6 +2,15 @@
  * If a function makes immutable updates to its arguments, it does not change 
  * the argument but instead makes a copy and changes that copy.
  */
+
+// BAD!!!
+const removeItemAtIndex = (list, index) => {
+    list.splice(index, 1);
+    return list;
+};
+console.log(removeItemAtIndex(['a', 'b', 'c', 'd'], 1));
+
+// GOOD!!!
 const removeItemAtIndex = (list, index) => {
     return [
         ...list.slice(0,index),
